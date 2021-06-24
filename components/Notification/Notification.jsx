55d4edc3c9a3bc43693e8d01462088ab5cde79e1
aryanvichare <aryanvichare10@gmail.com>
2021-06-23 22:10:59 -0700

    add participant and notification components

---
+++ components/Notification/Notification.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Notification.module.scss";
+
+const Notification = ({ type, name, message }) => {
+  switch (type) {
+    case "connect":
+      return (
+        <div className={styles.connectMessage}>{name} joined the party</div>
+      );
+
+    case "disconnect":
+      return (
+        <div className={styles.disconnectMessage}>{name} left the party!</div>
+      );
+
+    case "message":
+      return (
+        <div className={styles.userMessage}>
+          {name} says "{message}"
+        </div>
+      );
+
+    default:
+      return null;
+  }
+};
+
+export default Notification;
---
+++ components/Participant/Participant.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Participant.module.scss";
+
+const Participant = ({ name }) => {
+  return (
+    <div className={styles.participantContainer}>
+      <div className={styles.participantCircle}>
+        {name.substring(0, 1).toUpperCase()}
+      </div>
+      <div className={styles.participantName}>{name}</div>
+    </div>
+  );
+};
+
+export default Participant;
